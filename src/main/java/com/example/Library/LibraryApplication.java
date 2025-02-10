ff784b9f74b2222158c93a083028d611cc1f8899
4lloe <m4lyid4nil4@gmail.com>
2025-02-10 20:51:49 +0200

    h2 DB test connection

--- src/main/java/com/example/Library/LibraryApplication.java
+++ src/main/java/com/example/Library/LibraryApplication.java
@@ -1,17 +1,36 @@
 package com.example.Library;
 
+import com.example.Library.dao.service.BookService;
+import com.example.Library.entity.Book;
+import com.example.Library.spring.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@RestController
+@RequestMapping("application/json")
 public class LibraryApplication extends SpringBootServletInitializer {
+
+	@Autowired
+	private BookRepository bookRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
 	}
 
+
+	@RequestMapping("create-book")
+	public Book createBook() {
+		return bookRepository.save(new Book());
+	}
 }
+
+
